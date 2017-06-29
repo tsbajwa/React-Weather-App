07666afd63309d10b504ef191f53396921e8e187
tsbajwa <teranjit@gmail.com>
2017-06-29 17:23:28 +1000

    Fleshed out in prep for API calls

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
-export default function Forecast() {
-  return (
-    <div>
-      Forecast Div!
-    </div>
-  );
+class Forecast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weather: [],
+      loading: true,
+    };
+  }
+  componentDidMount = () => {
+    const city = this.props.location.search;
+    this.getWeather(city);
+  }
+
+  getWeather = (city) => {
+    console.log(city);
+  }
+
+  render() {
+    return (
+      <div>
+        Forecast div!!
+      </div>
+    );
+  }
 }
+
+module.exports = Forecast;
+
+// Do we need to use queryString module?
