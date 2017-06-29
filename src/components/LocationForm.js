684eb07d0cfa8718011acb8b40e1e1c68c9d7dd8
tsbajwa <teranjit@gmail.com>
2017-06-29 17:20:45 +1000

    Minor change to link search parameter

--- src/components/LocationForm.js
+++ src/components/LocationForm.js
@@ -17,7 +17,7 @@ class LocationForm extends React.Component {
           <input type='text' value={this.state.location} onChange={this.handleChange}></input>
           <Link to= {{
             pathname: '/forecast',
-            search: `?city=${this.state.location}`,
+            search: `${this.state.location}`,
           }}>Get Weather</Link>
       </div>
     );
