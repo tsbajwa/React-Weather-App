f43216c0922c1afca08ac206853d145188f1e687
tsbajwa <teranjit@gmail.com>
2017-07-05 13:23:05 +1000

    Added picture and city

--- src/components/Detailed.js
+++ src/components/Detailed.js
@@ -3,14 +3,19 @@ import React from 'react';
 export default function Detailed(props) {
   const weatherProp = props.location.state.dailyWeather;
   const { temp_min: tempMin, temp_max: tempMax, humidity } = weatherProp.main;
-  const description = weatherProp.weather[0].description;
+  const { description, icon } = weatherProp.weather[0];
   const city = weatherProp.city;
   const information = [city, description, tempMin, tempMax, humidity];
   return (
     <div>
-    {information.map((data, index) => <li key={index}>{data}</li>)}
+      <div>
+        <img src={`../../assets/weather-icons/${icon}.svg`}></img>
+        {city}
+      </div>
+      <div>
+        {information.map((data, index) => <li key={index}>{data}</li>)}
+      </div>
     </div>
   );
 }
 
-// Need to bring in location prop
