c4d1bef0c3d4154d9996d93b8aea561bd09f8173
tsbajwa <teranjit@gmail.com>
2017-07-20 14:14:08 +1000

    homepage styling complete

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,11 @@ import LocationForm from './LocationForm';
 
 export default function App() {
   return (
-    <div>
-      <h1>Enter City and State</h1>
-      <LocationForm />
+    <div className='home'>
+      <div>
+        <h1>Enter City and State</h1>
+        <LocationForm />
+        </div>
     </div>
   );
 }
