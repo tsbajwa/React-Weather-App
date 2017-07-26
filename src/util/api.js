d919522525d9113468a12cdae41bdc26a8a3b008
tsbajwa <teranjit@gmail.com>
2017-07-27 09:17:46 +1000

    Changed to metric units

--- src/util/api.js
+++ src/util/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Want a function to make the URL which we will make the call with
 
 export default function weatherDataRetreival(city) {
-  const URL = `http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&APPID=9b9bc8f9e7e2c636ea06d9c3a882724a&cnt=5`;
+  const URL = `http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&units=metric&APPID=9b9bc8f9e7e2c636ea06d9c3a882724a&cnt=5`;
   return axios.get(URL);
 }
 
